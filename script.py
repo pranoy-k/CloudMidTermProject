56bab2683d010c15f37393ead3af5bf94d0bf10a
kpranoy <kpranoy@tamu.edu>
2019-02-21 19:36:40 -0600

    Modify Script

--- script.py
+++ script.py
@@ -9,15 +9,17 @@ import time
 import argparse
 import multiprocessing
 import glob
+import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--num_processes', default=10, type=int, help='Number of Processes involved')
+parser.add_argument('-s', '--min', default=1, type=int, help='Starting Number of Processes involved')
+parser.add_argument('-e', '--max', default=10, type=int, help='Ending Number of Processes involved')
 parser.add_argument('-f', '--in_files_format', default='./file*', help='pattern to match text input files')
 args = parser.parse_args()
 
-queue = multiprocessing.Queue()
-queue.put(0)
-lock = multiprocessing.Lock()
+
+x_process = list(range(args.min, args.max + 1))
+y_metric = []
 
 def square_numbers_in_file(file):
     with open(file, 'r') as file:
@@ -25,19 +27,30 @@ def square_numbers_in_file(file):
             num = int(line)
             answer = num*num
 
-
 in_files = sorted(glob.glob(args.in_files_format))
 
-start = time.time()
+for processes in range(args.min, args.max + 1):
 
-print('Starting processing using %d processes' % args.num_processes)
+    print('Starting processing using %d processes' % processes)
 
-pool = multiprocessing.Pool(args.num_processes)
-pool.map_async(square_numbers_in_file, in_files)
-pool.close()
-pool.join()
-pool.terminate()
+    pool = multiprocessing.Pool(processes)
+    start = time.time()
+    pool.map_async(square_numbers_in_file, in_files)
+    pool.close()
+    pool.join()
+    pool.terminate()
 
-total_time = (time.time() - start)*1000
-print("Completed processing in %f milli_seconds" % total_time) 
+    total_time = (time.time() - start)*1000
+    y_metric.append(total_time)
+    print("Completed processing in %f milli_seconds" % total_time) 
+
+
+file_name = './Results/' + '_'.join(str(datetime.datetime.utcnow()).split()) + '.txt'
+
+with open(file_name, 'w') as file:
+    for x, y in zip(x_process, y_metric):
+        file.write(str(x) + ' '+  str(y) + '\n')
+
+
+
 
